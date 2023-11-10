10c3f3dff435c221ca43cf16b53a72b8cf846d7f
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-11-10 18:54:01 -0300

    :label: new interface to a position object and new method for the tictactoe object

--- src/game/models/Interfaces.ts
+++ src/game/models/Interfaces.ts
@@ -3,4 +3,10 @@ import { TictactoeGrid } from "./Types";
 export interface ITictactoe {
     getGrid(): TictactoeGrid
     setGrid(grid: TictactoeGrid): void
+    getWinner(): number
+}
+
+export interface ICellPos {
+    row: number
+    col: number
 }
